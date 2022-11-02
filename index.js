8e7a2401124c4c7c132513a6fc25c301e7c5b654
ghabocode <ghabo393443@gmail.com>
2022-11-01 18:41:27 -0700

    menu

---
+++ index.js
@@ -0,0 +1,5 @@
+const navLinks = document.querySelector('.nav-links')
+
+
+const navToggle = document.querySelector('.span-toggle');
+navToggle.addEventListener('click', () =>  navLinks.classList.toggle('flex'));
